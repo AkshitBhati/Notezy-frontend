37de2a5e800ea01c076c5346d6b711eb5d089d25
AkshitBhati <131599283+AkshitBhati@users.noreply.github.com>
2023-06-05 22:18:35 +0530

    Update NoteCard.jsx

--- src/components/Notespage/NoteCard/NoteCard.jsx
+++ src/components/Notespage/NoteCard/NoteCard.jsx
@@ -10,7 +10,8 @@ import {
   useDisclosure,
   VStack,
   useColorMode,
-  useColorModeValue
+  useColorModeValue,
+  Box
 } from "@chakra-ui/react";
 import "./style.css";
 
@@ -49,60 +50,69 @@ export default function NoteCard({ title, body, user, _id }) {
   const cardBg = useColorModeValue("#FFB4B4", 'gray.500')
 
   return (
-    <Card bg={cardBg} color={'white'}>
-      <CardBody>
-        <VStack>
-          <h1 style={{fontWeight:"600", fontFamily:"Roboto", fontSize:"20px"}}>{title}</h1>
-          <h3 style={{marginTop:"10px", fontWeight:"500" }}>{body}</h3>
+    <Card bg={cardBg} color="white">
+  <CardBody>
+    <Box textAlign="center">
+      <Heading as="h1" fontWeight="600" fontFamily="Roboto" fontSize={{ base: "20px", md: "24px", lg: "28px" }} mb={2}>
+        {title}
+      </Heading>
+      <Heading as="h3" fontWeight="500" fontSize={{ base: "16px", md: "18px", lg: "20px" }} my={2}>
+        {body}
+      </Heading>
+    </Box>
 
-          <Flex gap={2}>
-            <>
-              <Button onClick={onOpen} toast >Update</Button>
+    <Flex justifyContent="center" alignItems="center" mt={4}>
+      <>
+        <Button onClick={onOpen} mr={3}>
+          Update
+        </Button>
 
-              <Modal
-                initialFocusRef={initialRef}
-                finalFocusRef={finalRef}
-                isOpen={isOpen}
-                onClose={onClose}
-              >
-                <ModalOverlay />
-                <ModalContent>
-                  <ModalHeader>Update Note</ModalHeader>
-                  <ModalCloseButton />
-                  <ModalBody pb={6}>
-                    <Input
-                      value={tempTitle}
-                      m
-                      placeholder="Please enter title"
-                      onChange={(e) => setTitle(e.target.value)}
-                    ></Input>
-                    <Textarea
-                      mt={8}
-                      value={tempBody}
-                      placeholder={"Please enter description"}
-                      onChange={(e) => setBody(e.target.value)}
-                    ></Textarea>
-                  </ModalBody>
+        <Modal
+          initialFocusRef={initialRef}
+          finalFocusRef={finalRef}
+          isOpen={isOpen}
+          onClose={onClose}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Update Note</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <Input
+                value={tempTitle}
+                m
+                placeholder="Please enter title"
+                onChange={(e) => setTitle(e.target.value)}
+              ></Input>
+              <Textarea
+                mt={8}
+                value={tempBody}
+                placeholder="Please enter description"
+                onChange={(e) => setBody(e.target.value)}
+              ></Textarea>
+            </ModalBody>
 
-                  <ModalFooter>
-                    <Button colorScheme="blue" mr={3} onClick={updateNote}>
-                      Update
-                    </Button>
-                    <Button onClick={onClose}>Cancel</Button>
-                  </ModalFooter>
-                </ModalContent>
-              </Modal>
-            </>
-            <Button
-              onClick={() => {
-                dispatch(deleteNotes(_id));
-              }}
-            >
-              Delete
-            </Button>
-          </Flex>
-        </VStack>
-      </CardBody>
-    </Card>
+            <ModalFooter >
+              <Button colorScheme="blue" mr={3} onClick={updateNote}>
+                Update
+              </Button>
+              <Button onClick={onClose}>Cancel</Button>
+
+              
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+      <Button
+        onClick={() => {
+          dispatch(deleteNotes(_id));
+        }}
+      >
+        Delete
+      </Button>
+    </Flex>
+  </CardBody>
+</Card>
+
   );
 }
