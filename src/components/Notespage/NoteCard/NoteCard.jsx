df9c525e95d618c5c2c4c8797eaface867824d6d
Akshit Bhati <akshitworks24@gmail.com>
2023-06-21 13:51:53 +0530

    Update NoteCard.jsx

--- src/components/Notespage/NoteCard/NoteCard.jsx
+++ src/components/Notespage/NoteCard/NoteCard.jsx
@@ -27,8 +27,10 @@ import {
   ModalCloseButton,
 } from "@chakra-ui/react";
 import { useRef, useState } from "react";
-import { toast } from "react-toastify"
-export default function NoteCard({ title, body, user, _id }) {
+import { toast } from "react-toastify";
+
+
+export default function NoteCard({ title, body, user, _id, color }) {
   const dispatch = useDispatch();
   const [notes, setNotes] = useState([]);
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -38,81 +40,81 @@ export default function NoteCard({ title, body, user, _id }) {
   const [tempTitle, setTitle] = useState(title);
   const [tempBody, setBody] = useState(body);
 
-  
-  const updateNote =()=>{
+  const updateNote = () => {
+    dispatch(updateNotes(_id, { title: tempTitle, body: tempBody }));
+    onClose();
+    toast.success("Note updated successfully");
+  };
 
-    dispatch(updateNotes(_id,{title:tempTitle,body:tempBody}))
-    onClose()
-    toast.success('Note updated successfully')
-  }
-  
-  const { toggleColorMode } = useColorMode()
-  const cardBg = useColorModeValue("#FFB4B4", 'gray.500')
+  const { toggleColorMode } = useColorMode();
+  const cardBg = useColorModeValue("#FFB4B4", "gray.500");
 
   return (
-    <Card bg={cardBg} color="white">
-  <CardBody>
-    <Box textAlign="center">
-      <Heading as="h1" fontWeight="600" fontFamily="Roboto" fontSize={{ base: "20px", md: "24px", lg: "28px" }} mb={2}>
-        {title}
-      </Heading>
-      <Heading as="h3" fontWeight="500" fontSize={{ base: "16px", md: "18px", lg: "20px" }} my={2}>
-        {body}
-      </Heading>
-    </Box>
+    <Card background={color || cardBg}>
+      <CardBody>
+        <Box textAlign="center">
+          <Heading
+            as="h1"
+            fontWeight="600"
+            fontFamily="Roboto"
+            fontSize={{ base: "20px", md: "24px", lg: "28px" }}
+            mb={2}
+          >
+            {tempTitle}
+          </Heading>
+          <Heading
+            as="h3"
+            fontWeight="500"
+            fontSize={{ base: "16px", md: "18px", lg: "20px" }}
+            my={2}
+          >
+            {tempBody}
+          </Heading>
+        </Box>
 
-    <Flex justifyContent="center" alignItems="center" mt={4}>
-      <>
-        <Button onClick={onOpen} mr={3}>
-          Update
-        </Button>
+        <Flex justifyContent="center" alignItems="center" mt={4}>
+          <>
+            <Button onClick={onOpen} mr={3}>
+              Update
+            </Button>
 
-        <Modal
-          initialFocusRef={initialRef}
-          finalFocusRef={finalRef}
-          isOpen={isOpen}
-          onClose={onClose}
-        >
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Update Note</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody pb={6}>
-              <Input
-                value={tempTitle}
-                m
-                placeholder="Please enter title"
-                onChange={(e) => setTitle(e.target.value)}
-              ></Input>
-              <Textarea
-                mt={8}
-                value={tempBody}
-                placeholder="Please enter description"
-                onChange={(e) => setBody(e.target.value)}
-              ></Textarea>
-            </ModalBody>
+            <Modal
+              initialFocusRef={initialRef}
+              finalFocusRef={finalRef}
+              isOpen={isOpen}
+              onClose={onClose}
+            >
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>Update Note</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody pb={6}>
+                  <Input
+                    value={tempTitle}
+                    m
+                    placeholder="Please enter title"
+                    onChange={(e) => setTitle(e.target.value)}
+                  ></Input>
+                  <Textarea
+                    mt={8}
+                    value={tempBody}
+                    placeholder="Please enter description"
+                    onChange={(e) => setBody(e.target.value)}
+                  ></Textarea>
+                </ModalBody>
 
-            <ModalFooter >
-              <Button colorScheme="blue" mr={3} onClick={updateNote}>
-                Update
-              </Button>
-              <Button onClick={onClose}>Cancel</Button>
-
-              
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </>
-      <Button
-        onClick={() => {
-          dispatch(deleteNotes(_id));
-        }}
-      >
-        Delete
-      </Button>
-    </Flex>
-  </CardBody>
-</Card>
-
+                <ModalFooter>
+                  <Button colorScheme="blue" mr={3} onClick={updateNote}>
+                    Update
+                  </Button>
+                  <Button onClick={onClose}>Cancel</Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+          </>
+          <Button onClick={() => dispatch(deleteNotes(_id))}>Delete</Button>
+        </Flex>
+      </CardBody>
+    </Card>
   );
 }
