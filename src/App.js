f8facf35ace369efd473c31cb3d470189d822782
Akshit Bhati <akshitworks24@gmail.com>
2023-07-18 19:05:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <div className="App">
-      <Toaster
-        position="top-center"
-        toastOptions={{
-          duration: 2000,
-          style: {
-            fontSize: "1.2rem",
-            fontWeight: "bold",
-            background: "rgba(0, 0, 0, 0.8)",
-            color: "#fff",
-          },
-        }}
-      />
+      <ToastContainer
+				position="bottom-center"
+				autoClose={2000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				theme="colored"
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+			/>
       <Navbar />
       <AllRoutes />
       
